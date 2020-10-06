923e1f2d4dd95ae4c238aaa8b68a929333d4130c
Keshav Kapur <57453738+Blebot0@users.noreply.github.com>
2020-10-06 09:33:31 +0530

    Add files via upload

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -5,16 +5,25 @@ from geometry_msgs.msg import Twist, Point
 from turtlesim.msg import Pose
 import time
 flag = 0 
-final_x = 5.53
-final_y = 5.54
+final_x = 0
+final_y = 0
 x = 0
 y = 0
 
+
 def callback(dat):
-    global final_x, final_y, x, y
+    global final_x, final_y, x, y, flag
+    
     x = round(dat.x, 2) 
-    y = round(dat.y, 2)  
-    print(final_x,x, final_y, y)
+    y = round(dat.y, 2)
+
+    if flag == 0:
+        time.sleep(1)
+        final_x = x
+        final_y = y
+        flag = 1
+  
+    print("goal x: ", final_x, "position x: ", x,"goal y: ",  final_y,"position y: ",  y)
     
 
 
@@ -29,10 +38,12 @@ r = rospy.Rate(100)
 while not rospy.is_shutdown():
     
     if final_x == x and final_y == y:
-        speed.linear.x = 0
-        speed.angular.z = 0
-        pub.publish(speed)
-        break
+        if(flag == 1):
+            speed.linear.x = 0
+            speed.angular.z = 0
+            pub.publish(speed)
+            break
+        
 
     speed.linear.x = 0.4
     speed.angular.z = 0.4
