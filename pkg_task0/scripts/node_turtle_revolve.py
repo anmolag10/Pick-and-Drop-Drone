238660d65e29df587900f79abdf82ffa149591ab
Aditi Rao <57457307+aditirao7@users.noreply.github.com>
2020-10-06 11:13:21 +0530

    Update node_turtle_revolve.py

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist, Point
+from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import time
+
 flag = 0 
 final_x = 0
 final_y = 0
 x = 0
 y = 0
 
-
-def callback(dat):
+def turtle_pose(pose):
     global final_x, final_y, x, y, flag
     
-    x = round(dat.x, 2) 
-    y = round(dat.y, 2)
+    x = round(pose.x, 1) 
+    y = round(pose.y, 1)
 
     if flag == 0:
-        time.sleep(1)
+        time.sleep(0.5)
         final_x = x
         final_y = y
         flag = 1
-  
-    print("goal x: ", final_x, "position x: ", x,"goal y: ",  final_y,"position y: ",  y)
-    
 
+def turtle_revolve():
+	rospy.init_node('node_turtle_revolve', anonymous=True)
 
+	rospy.Subscriber("/turtle1/pose", Pose, turtle_pose)
 
-rospy.init_node('turtle_rotate', disable_signals=True)
+	pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 10)
+	vel = Twist()
 
-rospy.Subscriber("/turtle1/pose", Pose, callback)
-pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 10)
-speed = Twist()
-r = rospy.Rate(100)
+	rate = rospy.Rate(100)
+	init_time = rospy.get_time()
 
-while not rospy.is_shutdown():
-    
-    if final_x == x and final_y == y:
-        if(flag == 1):
-            speed.linear.x = 0
-            speed.angular.z = 0
-            pub.publish(speed)
-            break
-        
+	while not rospy.is_shutdown():
+	    
+	    if final_x == x and final_y == y:
+		if(flag == 1):
+		    vel = Twist()
+		    pub.publish(vel)
+		    rospy.loginfo("goal reached")
+		    break
+		
+	    rospy.loginfo("Moving in a circle\n"+ str(rospy.get_time() - init_time))
+	    vel.linear.x = 2.0
+	    vel.angular.z = 2.0
+	    pub.publish(vel)
 
-    speed.linear.x = 0.4
-    speed.angular.z = 0.4
-    pub.publish(speed)
+	    rate.sleep()
 
-    r.sleep()
+if __name__ == '__main__':
+	turtle_revolve() 
