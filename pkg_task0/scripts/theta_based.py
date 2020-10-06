4e12a76340708192db1242c369151fa12c062663
Aditi Rao <57457307+aditirao7@users.noreply.github.com>
2020-10-06 13:48:31 +0530

    Update theta_based.py

--- pkg_task0/scripts/theta_based.py
+++ pkg_task0/scripts/theta_based.py
@@ -5,35 +5,35 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import math
 
-theta=0
+theta=0 #global variable so it can be accessed in turtle_revolve
 
-def turtle_pose(pose):
+def turtle_pose_callback(pose):
 	global theta
-	theta = pose.theta if pose.theta>=0 else pose.theta + 2*math.pi
+	theta = pose.theta if pose.theta>=0 else pose.theta + 2*math.pi #Makes arc angle theta range from 0 to 2*pi radians
 
 def turtle_revolve():
-	rospy.init_node('node_turtle_revolve', anonymous=True)
+	rospy.init_node('node_turtle_revolve', anonymous=True) #Initialising the node
 
-	rospy.Subscriber("turtle1/pose", Pose, turtle_pose)
+	rospy.Subscriber("turtle1/pose", Pose, turtle_pose_callback) #Subscribing to the pose of the turtle
 
-	pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 10)
-	vel = Twist()
-	vel.linear.x = 1.0
-	vel.angular.z = 1.0
-	radius = vel.linear.x / vel.angular.z
+	pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 10) #Specifying the topic where velocity is to be published
+	vel = Twist() #Initialising velocity message
+	vel.linear.x = 1.0 #Assigning 1 m/s as linear velocity 
+	vel.angular.z = 1.0 #Assigning 1 rad/s as angular velocity
+	radius = vel.linear.x / vel.angular.z 
 
 	rate = rospy.Rate(100)
 
-	while theta < 2*math.pi - 0.04: #
-		rospy.loginfo("Moving in a circle\n"+ str(radius*theta))
-		pub.publish(vel)
+	while theta < 2*math.pi - 0.02: #Move while angle of arc travelled is less than 2*pi radians (considering a buffer of 0.02 radians to make sure turtle stops at the appropriate position)
+		rospy.loginfo("Moving in a circle\n"+ str(radius*theta)) #Printing distance info on terminal 
+		pub.publish(vel) #Publishing velocity
 		rate.sleep()
 	
-	vel=Twist()
+	vel=Twist() #Reinitialising velocity message to 0 so that the turtle stops
 	pub.publish(vel)
 	rospy.loginfo("goal reached")
 
 	rospy.spin()
 
 if __name__ == '__main__':
-	turtle_revolve() 
+	turtle_revolve() #Calling the function to make the turtle revolve
