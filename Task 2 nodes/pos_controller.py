3b51327fba86e6841faf67854cb10c0d6e4cbb68
Aditi Rao <57457307+aditirao7@users.noreply.github.com>
2020-11-15 15:34:54 +0530

    Add files via upload

---
+++ Task 2 nodes/pos_controller.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+# Importing the required libraries
+
+from vitarana_drone.msg import *
+from sensor_msgs.msg import NavSatFix, LaserScan
+from std_msgs.msg import Float32
+import rospy
+import numpy as np
+import math
+import time
+
+
+class Position():
+
+    def __init__(self):
+        # initializing ros node with name position_controller
+        rospy.init_node('node_position_controller')
+
+        # Numpy array for GPS coordinate setpoints
+        self.setpoints = np.array(
+            [[4.0, 0.0, 3.0], [19.0000451704, 72.0, 0.31]])
+
+        # Numpy array for current GPS location
+        self.currentloc = np.array([0.0, 0.0, 0.0])
+        # Index of GPS setpoint that the drone is headed towards
+        self.loc = 0
+        # Count for Stability
+        self.stabilize = 0
+        # Drone Command message of type edrone_cmd and initialisation
+        self.setpoint_rpy = edrone_cmd()
+        self.setpoint_rpy.rcRoll = 0
+        self.setpoint_rpy.rcPitch = 0
+        self.setpoint_rpy.rcYaw = 0
+        self.setpoint_rpy.rcThrottle = 0
+
+        # Numpy array for PID gains : [Latitude, Longitude, Altitude]
+        # Coefficient ratios for Pid[Latitude] [Kp, Ki, Kd] : [6000, 0.08, 12000]
+        # Coefficient ratios for Pid[Longitude] [Kp, Ki, Kd] : [6000, 0.8, 12000] 
+        # Coefficient ratios for Pid[Altitude] [Kp, Ki, Kd] : [0.6, 0.008, 0.3]
+        
+        # Value of [Kp, Ki, Kd][Latitude] : [720, 55, 1750]
+        # Value of [Kp, Ki, Kd][Longitude] : [720, 55, 1650]
+        # Value of [Kp, Ki, Kd][Altitude] : [225, 4, 465]
+        self.Kp = np.array([20 * 0.006, 20 * 0.006, 225 * 0.6])
+        self.Ki = np.array([4 * 0.008, 4 * 0.008, 4 * 0.008])
+        self.Kd = np.array([60 * 0.03, 40 * 0.03, 465 * 0.3])
+
+        # For storing previous error for derivative term
+        self.prev_values = np.array([0.0, 0.0, 0.0])
+        # For storing sum of error for integral term
+        self.integral = np.array([0.0, 0.0, 0.0])
+        # Maximum and Minimum values for roll, pitch and throttle commands
+        self.max_value = 2000
+        self.min_value = 1000
+
+        # PID sampling rate and time
+        self.sample_rate = 10  # in Hz
+        self.sample_time = 0.1  # in seconds
+
+        # Publishing /edrone/drone_command, /altitude_error, /latitude_error,
+        # /longitude_error
+        self.setpoint_pub = rospy.Publisher(
+            '/edrone/drone_command', edrone_cmd, queue_size=1)
+        self.altitude_error_pub = rospy.Publisher(
+            '/altitude_error', Float32, queue_size=1)
+        self.latitude_error_pub = rospy.Publisher(
+            '/latitude_error', Float32, queue_size=1)
+        self.longitude_error_pub = rospy.Publisher(
+            '/longitude_error', Float32, queue_size=1)
+
+        # Subscribing to /edrone/gps for current GPS location
+        rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
+	rospy.Subscriber('/edrone/range_finder_top', LaserScan, self.lidar_callback)
+        # ------------------------------------------------------------------------------------------------------------
+
+    # Callback for GPS location
+    def gps_callback(self, msg):
+        self.currentloc = np.array([msg.latitude, msg.longitude, msg.altitude])
+	
+    def lidar_callback(self, msg):
+	self.ranges = msg.ranges
+
+    # Function for checking limits of PID output
+    def checkLimits(self, drone_command):
+        if drone_command > self.max_value:
+            return self.max_value
+        elif drone_command < self.min_value:
+            return self.min_value
+        else:
+            return drone_command
+
+    def lat_to_x(self, input_latitude):
+	return 110692.0702932625 * (input_latitude - 19)
+
+    def long_to_y(self, input_longitude):
+	return -105292.0089353767 * (input_longitude - 72)
+    # if you use this for control, you may have to change the relevant pitch   direction because of the sign
+
+    def gps_to_xy(self, input_arr):
+	input_arr[0] = self.lat_to_x(input_arr[0])
+	input_arr[1] = self.long_to_y(input_arr[1])
+	return input_arr
+    
+    def align_to_goal(self):
+	theta = math.degrees(math.atan2(self.long_to_y(self.setpoints[0,1]), self.lat_to_x(self.setpoints[0,0])))
+	theta = theta if theta >= 0 else theta + 360
+	theta = (theta + 270) % 360
+	theta = theta if theta < 180 else theta - 360
+	theta = (theta + 540) / 0.36
+	return theta
+
+    # PID algorithm
+    def pid(self):
+        # Initially, GPS location is not updated,i.e., [0,0,0]
+        # To avoid undesired motion due to this the following is used.
+        # It returns to the main function if any values are 0
+        if not np.any(self.currentloc):
+            return
+
+        # Calculating error term and rounding off to 7 decimal points
+        # Rounding off because double precision is overkill for PID
+        error = np.round((self.setpoints[self.loc] - self.gps_to_xy(self.currentloc)), 7)
+	print(error)
+
+        # Calculating derivative term and rounding off
+        # / symbol allows divison for sample_time scalar with every element in the array
+        derivative = np.round(
+            ((error - self.prev_values) / self.sample_time), 7)
+
+        # Calculating integral term and rounding off
+        # * symbol allows multiplication for sample_time scalar with every element in the array
+        self.integral = np.round(
+            (self.integral + error * self.sample_time), 7)
+
+        # Calculating PID output and rounding off
+        # * symbol for numpy arrays allows multiplication of the corresponding elements
+        # output = [out_latitude, out_longitude, out_altitude]
+        output = np.round(
+            ((self.Kp * error) + (self.Ki * self.integral) + (self.Kd * derivative)), 7)
+
+        # Calculating throttle, roll and pitch and checking limits
+        # Since 1500 is the mid value,
+        # Direction will change according to sign of output
+        throttle = self.checkLimits(1500.0 + output[2])
+        pitch = self.checkLimits(1500.0 + output[1])
+        roll = self.checkLimits(1500.0 + output[0])
+
+        # Assigning prev_values with error for the next iteration
+        self.prev_values = error
+
+        # Publishing errors for Plotjuggler
+        self.latitude_error_pub.publish(error[0])
+        self.longitude_error_pub.publish(error[1])
+        self.altitude_error_pub.publish(error[2])
+        '''
+        # Checking condition for threshold box of setpoints
+        if abs(
+            error[1]) < 0.0000047487 and abs(
+            error[0]) < 0.000004517 and abs(
+                error[2]) < 0.2:
+            # Checking if drone has reached threshold box of final setpoint
+            # Publishing stop signal (throttle = 1000) for drone to land
+            if self.loc == 2:
+                self.setpoint_rpy.rcThrottle = 1000
+                self.setpoint_rpy.rcRoll = 1500
+                self.setpoint_rpy.rcPitch = 1500
+                self.setpoint_rpy.rcYaw = 1500
+                self.setpoint_pub.publish(self.setpoint_rpy)
+                return
+            # Counting till 10 using stabilize variable for drone to stabilize
+            # at setpoint
+            if self.stabilize < 10:
+                self.stabilize = self.stabilize + 1
+                return
+            # Increasing value of location index so that next GPS setpoint is
+            # set
+            self.loc = self.loc + 1
+            # Reinitialising stabilize to 0 so it can be used for next setpoint
+            self.stabilize = 0'''
+
+        # Publishing final PID output on /edrone/drone_command for the attitude
+        # controller
+        self.setpoint_rpy.rcRoll = roll
+        self.setpoint_rpy.rcPitch = pitch
+        self.setpoint_rpy.rcYaw = 1500
+        self.setpoint_rpy.rcThrottle = throttle
+        self.setpoint_pub.publish(self.setpoint_rpy)
+
+# ------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+
+    e_drone_position = Position()
+    # Defining rospy rate such that PID algorithm loops at the
+    # desired sampling rate
+    r = rospy.Rate(e_drone_position.sample_rate)
+    while not rospy.is_shutdown():
+        # Calling PID function
+        e_drone_position.pid()
+        r.sleep()
