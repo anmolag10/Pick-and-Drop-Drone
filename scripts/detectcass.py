d603202c283a7cf6b546d23af6e847174127f845
Aditi Rao <57457307+aditirao7@users.noreply.github.com>
2020-12-16 17:13:52 +0530

    Update detectcass.py

--- scripts/detectcass.py
+++ scripts/detectcass.py
@@ -37,7 +37,7 @@ class image_proc():
         self.y_quad=0
         self.z_m=0
         self.lat= 0
-        self.confirmation_msg = "False,0.0,0.0,0.0,0.0"
+        self.confirmation_msg = "False,0.0,0.0"
         self.detect_confirm_pub = rospy.Publisher(
             '/detect_confirm', String, queue_size=1)
         self.x_pub=rospy.Publisher('/edrone/err_x_m',Float32,queue_size=1)
@@ -48,45 +48,33 @@ class image_proc():
        msgstr=msg.data.split(',')
        self.x_quad=float(msgstr[0])
        self.y_quad=float(msgstr[1])
-       print(msgstr)
+
     def globalheight(self, msg):
         self.z_m=msg.ranges[0]
-        print(self.z_m)
     
-   
-        
-
-        
-
     def imdecode(self):
         global flag
-        self.logo_cascade = cv2.CascadeClassifier('/home/anmol/catkin_ws/src/vitarana_drone/scripts/data/cascade.xml')
+        self.logo_cascade = cv2.CascadeClassifier('/home/rohan/catkin_ws/src/vitarana_drone/scripts/data/cascade.xml')
         time.sleep(0.05)
         
         logo = self.logo_cascade.detectMultiScale(self.gray, scaleFactor=1.05)
             
         if len(logo) is not 0:
-                self.confirmation_msg = "True,"+str(logo[0,0])+","+str(logo[0,1])+","+str(logo[0,2])+","+str(logo[0,3])
                 for (x, y, w, h) in logo:
                       cv2.rectangle(self.img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                      if flag == 0:
-                          flag = 1
-                          self.x_center = x + ( w - x) / 2
-                          self.y_center = y + (h - y) / 2
-            
-  
+                      self.x_center = x + w/2
+                      self.y_center = y + h/2
+	self.confirmation_msg = "True,"+str(self.x_center)+","+str(self.y_center)
         self.detect_confirm_pub.publish(self.confirmation_msg)
-        print(self.confirmation_msg)
 
     def error_finder(self):
         focal_length=(200)/math.tan(1.3962634/2)
-        self.x_err=(self.x_center*self.z_m)/focal_length-self.x_quad
-        self.y_err=(self.y_center*self.z_m)/focal_length-self.y_quad
+        self.x_err=self.x_center*self.z_m/focal_length
+        self.y_err=self.y_center*self.z_m/focal_length
+	print(self.x_err, self.y_err)
         self.x_pub.publish(self.x_err)
         self.y_pub.publish(self.y_err)
 
-        
-      
     def image_callback(self, data):
         try:
             # Converting the image to OpenCV standard image
@@ -95,7 +83,8 @@ class image_proc():
         except CvBridgeError as e:
             print(e)
             return
-        cv2.imshow('pic', self.img)
+        plt.imshow(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB))
+	plt.show()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
 
